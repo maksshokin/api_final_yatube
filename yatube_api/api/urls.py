4f80b270fffbe8b1d4a42df98d324edee8aad26b
Maksim Shokin <maksshokin90@gmail.com>
2024-12-06 18:05:50 +0300

    Попытка 1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework import routers
 
 from api.views import CommentViewSet, FollowViewSet, PostViewSet, GroupViewSet
@@ -21,13 +22,9 @@ urlpatterns = [
     path('', include(v1_router.urls)),
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
