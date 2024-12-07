8b867b198d7651eb4662d9b9f202559f7ff14a04
Maksim Shokin <maksshokin90@gmail.com>
2024-12-07 09:49:07 +0300

    Второе ревью

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -64,6 +64,8 @@ class Post(models.Model):
     def __str__(self):
         return self.text
 
+    class Meta:
+        ordering = ['pub_date']
 
 class Comment(models.Model):
     author = models.ForeignKey(
